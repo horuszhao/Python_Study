ed910c472b3d58371da530b557a89472d2c10b6f
horuszhao <zhaoyun413@163.com>
2019-04-18 23:25:01 +0800

    init app

---
+++ src/learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\workspace\python\python_study\src\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
