4da16834c1068f0b6021631f65b0dafd3ec9cb59
horuszhao <zhaoyun413@163.com>
2019-04-28 17:43:05 +0800

    remove learning_log

--- src/learning_log/ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\workspace\python\python_study\src\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
