2f4841a861fe1bea303c08648a3ceffd7433d04e
horuszhao <zhaoyun413@163.com>
2019-05-04 00:01:05 +0800

    django topics

---
+++ src/learning_log/learning_log/learning_logs/urls.py
@@ -0,0 +1,9 @@
+"""定义learning_logs的URL模式"""
+from django.urls import path
+from . import views
+
+app_name="learning_logs"
+urlpatterns=[
+    path('', views.index, name='index'),
+    path("topics/",views.topics,name='topics'),
+]
