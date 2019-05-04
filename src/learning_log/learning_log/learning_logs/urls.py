77657f0f7200fbad3111c402b82319473f82071d
horuszhao <zhaoyun413@163.com>
2019-05-04 12:06:12 +0800

    add topic detail page

--- src/learning_log/learning_log/learning_logs/urls.py
+++ src/learning_log/learning_log/learning_logs/urls.py
@@ -6,4 +6,5 @@ app_name="learning_logs"
 urlpatterns=[
     path('', views.index, name='index'),
     path("topics/",views.topics,name='topics'),
+    path("topics/<int:topic_id>/",views.topic,name='topic')
 ]
